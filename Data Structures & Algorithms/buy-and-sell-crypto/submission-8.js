f8dad9f771f5b48277777ba58d08aed09d1d6e5b
Marko <59150246+MarkoNikolicViser@users.noreply.github.com>
2026-04-24 00:51:23 +0200

    Add: buy-and-sell-crypto - submission-8

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-8.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let left=0
+        let right=1
+        let res=0
+        while(right<prices.length){
+            if(prices[left]<prices[right]){
+                res=Math.max(res,prices[right]-prices[left])
+                right++
+            }else{
+               left=right
+                right++
+            }
+        }
+        return res
+    }
+}
